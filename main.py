2a0b31e795ce432690fa44b9f9257a688fd944a5
Nathaniel Jennings <headed2iu@gmail.com>
2023-02-19 13:11:47 -0500

    Semantics

--- main.py
+++ main.py
@@ -3,17 +3,6 @@ from xml.etree import ElementTree
 from xml.etree.ElementTree import Element
 from loguru import logger
 
-parser = argparse.ArgumentParser()
-parser.add_argument("property")
-parser.add_argument("value")
-parser.add_argument('-f', "--folder", default="C:/Program Files (x86)/Steam/steamapps/workshop/content/289070",
-                    required=False)
-
-args = parser.parse_args()
-
-mods_folder = args.folder
-property_path = args.property
-property_value = args.value
 
 def make_element_at_path(xml_tree: Element, element_path: str, element_value):
     sections = element_path.split('/')
@@ -35,17 +24,29 @@ def make_element_at_path(xml_tree: Element, element_path: str, element_value):
         make_element_at_path(xml_tree.find(section), '/'.join(sections[1:]), element_value)
 
 
-if __name__ == 
+if __name__ == '__main__':
 
-for folder in os.listdir(mods_folder):
-    absolute_folder_path = os.path.join(mods_folder, folder)
-    mod_info_path = sorted(pathlib.Path(absolute_folder_path).glob('*.modinfo'))[0]
-    mod_info_xml = ElementTree.parse(mod_info_path)
-    mod_info_xml_root = mod_info_xml.getroot()
-    mod_name = mod_info_xml_root.find("./Properties/Name").text
+    parser = argparse.ArgumentParser()
+    parser.add_argument("property")
+    parser.add_argument("value")
+    parser.add_argument('-f', "--folder", default="C:/Program Files (x86)/Steam/steamapps/workshop/content/289070",
+                        required=False)
 
-    xml_property = mod_info_xml.find(property_path)
-    make_element_at_path(mod_info_xml_root, property_path, property_value)
-    mod_info_xml.write(mod_info_path, xml_declaration=True, encoding='utf-8')
+    args = parser.parse_args()
 
-    logger.info(f"Updated {mod_name}: {property_path} -> {property_value}")
+    mods_folder = args.folder
+    property_path = args.property
+    property_value = args.value
+
+    for folder in os.listdir(mods_folder):
+        absolute_folder_path = os.path.join(mods_folder, folder)
+        mod_info_path = sorted(pathlib.Path(absolute_folder_path).glob('*.modinfo'))[0]
+        mod_info_xml = ElementTree.parse(mod_info_path)
+        mod_info_xml_root = mod_info_xml.getroot()
+        mod_name = mod_info_xml_root.find("./Properties/Name").text
+
+        xml_property = mod_info_xml.find(property_path)
+        make_element_at_path(mod_info_xml_root, property_path, property_value)
+        mod_info_xml.write(mod_info_path, xml_declaration=True, encoding='utf-8')
+
+        logger.info(f"Updated {mod_name}: {property_path} -> {property_value}")
